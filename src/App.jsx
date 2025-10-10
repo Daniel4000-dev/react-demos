e5f09048384492c4afe0e9f147212cc7e011a258
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 18:38:39 +0100

    create components for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import Card from "./components/Card";
-import Person from "./components/Person";
-import Product from "./components/Product";
-import ProductInfo from "./components/ProductInfo";
-import ProductList from "./components/ProductList";
-import UserList from "./components/UserList";
+const Cart = () => {
+  const items = ["Apple", "Banana", "Orange"]
 
-const ValidPassword = () => <h1>Valid Password</h1>
-const InvalidPassword = () => <h1>Invalid Password</h1>
+  return <div>
+    <h1>Cart</h1>
+    {items.length > 2 && <h2>You have {items.length} items in your cart</h2>}
 
-const Password = ({isValid}) =>
-  isValid ? <ValidPassword /> : <InvalidPassword />
+    <ul>
+      <h4>Products</h4>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  </div>
+};
+
 
 function App () {
   return (
     <>
-    <Password isValid={false} />
+    <Cart />
     </>
   )
 }

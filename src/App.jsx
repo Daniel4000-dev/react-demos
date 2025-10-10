aa5b652903a89b9aa7bf1a472f71bd8254e0f1b8
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 17:11:43 +0100

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import Add from "./components/Add";
-import Footer from "./components/Footer";
-import Greet from "./components/Greet";
-import Header from "./components/Header";
-
 function App () {
   return (
-   <div>
-    <Header />
-    <Greet />
-    <Add />
-    <Footer />
-   </div>
+   <section id="section">
+    <form>
+      <label htmlFor='name'>Name</label>
+      <input type='text' placeholder="Enter your name" />
+    </form>
+   </section>
   )
 }
 

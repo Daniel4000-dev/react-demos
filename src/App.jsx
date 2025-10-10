51892fa96c425487acba6b412a7028815ce886bf
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 17:14:28 +0100

    createdjsx rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
+import JSXRules from "./components/JSXRules";
+
 function App () {
   return (
-   <section id="section">
-    <form>
-      <label htmlFor='name'>Name</label>
-      <input type='text' placeholder="Enter your name" />
-    </form>
-   </section>
+   <JSXRules />
   )
 }
 

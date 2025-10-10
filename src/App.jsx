720035a1eca328aef24bfe56644c0044dde83128
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 18:29:49 +0100

    create components for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ const ValidPassword = () => <h1>Valid Password</h1>
 const InvalidPassword = () => <h1>Invalid Password</h1>
 
 const Password = ({isValid}) => {
-  if (isValid) {
-    return <ValidPassword />
-  }
+  // if (isValid) {
+  //   return <ValidPassword />
+  // }
 
-  return <InvalidPassword />;
+  // return <InvalidPassword />;
+  return isValid ? <ValidPassword /> : <InvalidPassword />
 }
 
 function App () {

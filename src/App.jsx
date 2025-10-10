17ce6c33b1a924efb48db849aabffd7eb75f6042
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 18:09:58 +0100

    create components for props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import ProductList from "./components/ProductList";
 import UserList from "./components/UserList";
 
 function App () {
-  return (
-    <>
-   <UserList />
-   <ProductList />
-   </>
-  )
+  return <User 
+  name="Big Baan" 
+  age={22} 
+  isMarried={false} 
+  hobbies={["coding", "sleepong", "readgin"]} 
+  img="https://via.placeholder.com/150"
+  alt="User Avatar"
+  />
+}
+
+const User = ({img, name, age, isMarried, hobbies, alt }) => {
+return <section>
+  <img src={img} alt={alt} width={200}/>
+  <h1>Name: {name}</h1>
+  <h2>Age: {age}</h2>
+  <h2>Marital Status: {isMarried ? "Married" : "Single"}</h2>
+  <h3>Hobbies: {hobbies}</h3>
+</section>
 }
 
 export default App;

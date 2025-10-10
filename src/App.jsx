dccfe5a90ffcb319bc5a333bcd9b29c12787d816
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 18:29:23 +0100

    create components for conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import ProductInfo from "./components/ProductInfo";
 import ProductList from "./components/ProductList";
 import UserList from "./components/UserList";
 
+const ValidPassword = () => <h1>Valid Password</h1>
+const InvalidPassword = () => <h1>Invalid Password</h1>
+
+const Password = ({isValid}) => {
+  if (isValid) {
+    return <ValidPassword />
+  }
+
+  return <InvalidPassword />;
+}
+
 function App () {
   return (
     <>
-    <Card>
-      <h1>My Card</h1>
-      <p>This is some content for card 1</p>
-    </Card>
-    <Card>
-      <h1>My Card</h1>
-      <p>This is some content for card 2</p>
-    </Card>
-    <Card>
-      <h1>My Card</h1>
-      <p>This is some content for card 3</p>
-    </Card>
+    <Password isValid={false} />
     </>
   )
 }

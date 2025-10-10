e764b5a06ed20617a51d1f6002789e7325730cc7
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 17:39:59 +0100

    create components for dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import ProductInfo from "./components/ProductInfo";
 
 function App () {
+  const numbers = [1, 2, 3, 4, 5]
+  const usersInfo = [
+    {
+      username: "Big Baan",
+      email: "test@gmail.com",
+      location: "India"
+    },
+     {
+      username: "Big Baan",
+      email: "test@gmail.com",
+      location: "Indonasia"
+    },
+     {
+      username: "Big Baan",
+      email: "test@gmail.com",
+      location: "France"
+    }
+
+  ]
 
   return (
-   <ProductInfo />
+   <main>
+    {numbers.map(number => (
+      <ul key={number}>
+        <li>{number}</li>
+      </ul>
+    ))}
+    {usersInfo.map((user) => (
+      <ul key={Math.random()}>
+        <li>{user.username}</li>
+        <li>{user.email}</li>
+        <li>{user.location}</li>
+      </ul>
+    ))}
+   </main>
   )
 }
 

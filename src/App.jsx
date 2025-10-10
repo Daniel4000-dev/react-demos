56092e7c55d4ee33253f4ad6a0cb55f9ba7cdb81
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-10 17:19:07 +0100

    create components for carring out expressions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 function App () {
   const myName = "John Doe";
-  
+  const multiply = (a, b) => a * b;
+
   return (
    <section id="section">
     <p>{2 + 2}</p>
 
     <h1>{myName}</h1>
+    <p>My Friend List: {["Alex", "John", "Waheed", "Jordan"]}</p>
+    <p>2 * 10 = {multiply(2, 2)}</p>
    </section>
   )
 }

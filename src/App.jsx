d53cb11992cecc092a1d8eaed240209acd77f74e
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-11 19:44:18 +0100

    create components for inline styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-const Weather = ({ temperature }) => {
-  if (temperature < 15) {
-    return <div>It's cold outside!</div>;
-  } else if (temperature >= 15 && temperature <= 25) {
-    return <div>The weather is moderate.</div>;
-  } else {
-    return <div>it's hot outside</div>;
-  }
-};
-
-const UserStatus = ({ loggedIn, isAdmin }) => {
-  return (
-    <>
-    {loggedIn && isAdmin ? "Welcome, Admin!" : loggedIn ? "Welcome, User!" : "Please log in."}
-    </>
-  )
-}
-
-const Greeting = ({ timeOfDay }) => {
-  return (
-    <>
-    {timeOfDay === 'morning' && <h1>Good Morning</h1>}
-    {timeOfDay === 'afternoon' && <h1>Good Afternoon!</h1>}
-    </>
-  )
-}
-
 function App() {
+  const styles = { color: "white", backgroundColor: "teal", padding: "2rem" };
   return (
-    <>
-      <Weather temperature={25} />
-      <UserStatus loggedIn={true} isAdmin={true} />
-      <Greeting timeOfDay="afternoon" />
-    </>
+    <section>
+      <h1 style={styles}>Inline Style</h1>
+    </section>
   );
 }
 

a754e0a151af648288963e87f3d6113212347193
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-13 14:51:08 +0100

    built component for style card

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { FaComputer } from "react-icons/fa6"
 import './index.css'
 import StyleCard from "./components/StyleCard";
 import ProfileCard from "./components/ProfileCard";
+import IconComponent from "./components/IconComponent";
 
 function App() {
   return (
     <section>
       <StyleCard />
       <ProfileCard />
+      <IconComponent />
     </section>
   );
 }
---
+++ src/components/IconComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaBeer } from 'react-icons/fa'
+
+const IconComponent = () => {
+  return (
+    <div>
+     <FaBeer />
+      <FaBeer size={30} />
+    </div>
+  )
+}
+
+export default IconComponent

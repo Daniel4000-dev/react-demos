db05448970ea657d183eaf066d85a93f27a95f49
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-13 15:02:48 +0100

    built component for copy handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const Copy = () => {
   const copyHandler = () => console.log("Don't copy my content!");
   return (
     <p onCopy={copyHandler}>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui est ducimus
+      Lorem, ipsum dolo sit amet consectetur adipisicing elit. Qui est ducimus
       quam sapiente sunt. Veritatis perferendis culpa quasi porro! Aperiam vel
       optio laudantium amet ullam laborum eaque impedit quia omnis?
     </p>

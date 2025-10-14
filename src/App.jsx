90a920132d26013b2dc8c1556bf202e84c6d40ec
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-14 09:49:43 +0100

    built component for useState management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 const state = "states are used to store and manage data that chnage over time and affect how components are rendered"
 const hooks = "hooks are special functions added in react 16.8 that allow you to use state and other React features in functional components"
+const useState = "useState hook allows us to track state in a functional component. state generally refers to data or properties that need to be tracked in an application."
 function App() {
   return (
     <section>

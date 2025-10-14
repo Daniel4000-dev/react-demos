f5529aa290dc75cc336384efbd3a256873d8f7bf
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-14 09:47:30 +0100

    built component for state and hooks definition

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-const Move = () => {
-  function moveHandler() {
-    alert('Mouse move event fired')
-    console.log('mouse move event fired')
-  }
-
-  return (
-    <p onMouseMove={moveHandler}>
-      Lorem, ipsum dolo sit amet consectetur adipisicing elit. Qui est ducimus
-      quam sapiente sunt. Veritatis perferendis culpa quasi porro! Aperiam vel
-      optio laudantium amet ullam laborum eaque impedit quia omnis?
-    </p>
-  );
-};
-
+const state = "states are used to store and manage data that chnage over time and affect how components are rendered"
+const hooks = "hooks are special functions added in react 16.8 that allow you to use state and other React features in functional components"
 function App() {
   return (
     <section>
-      <Move />
     </section>
   );
 }

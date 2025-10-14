1dedefadf5cc3ea361cd50c751e3b11b4913852b
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-14 10:02:38 +0100

    built component for useState management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+'use client'
+
+import { useState } from "react"
+
 const state = "states are used to store and manage data that chnage over time and affect how components are rendered"
 const hooks = "hooks are special functions added in react 16.8 that allow you to use state and other React features in functional components"
-const useState = "useState hook allows us to track state in a functional component. state generally refers to data or properties that need to be tracked in an application."
+const usestate = "useState hook allows us to track state in a functional component. state generally refers to data or properties that need to be tracked in an application."
 function App() {
+  const [count, setCount] = useState(0);
+   console.log(count)
+   const increment = () => setCount(count + 1)
+   const decrement = () => setCount(count - 1)
   return (
     <section>
+     <h1>{count}</h1>
+     <button onClick={increment}>+</button>
+     <button onClick={decrement}>-</button>
     </section>
   );
 }

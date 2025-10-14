2549fa160b81a75659392f3b8d7b9c86b70f9d51
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-14 10:26:28 +0100

    built component for useState management

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ const state = "states are used to store and manage data that chnage over time an
 const hooks = "hooks are special functions added in react 16.8 that allow you to use state and other React features in functional components"
 const usestate = "useState hook allows us to track state in a functional component. state generally refers to data or properties that need to be tracked in an application."
 function App() {
-  const [count, setCount] = useState(0);
-   console.log(count)
-   const increment = () => setCount(count + 1)
-   const decrement = () => setCount(count - 1)
+  const [friends, setFriends] = useState(['Alex', 'John']);
+  const addOne = () => setFriends([...friends, 'Baan']);
+  const removeOne = () => setFriends(friends.filter((f) => f != "John"));
+  const updateOne = () => {
+    setFriends(friends.map((f) => f === 'Alex' ? 'Alex Smith' : f))
+  }
   return (
     <section>
-     <h1>{count}</h1>
-     <button onClick={increment}>+</button>
-     <button onClick={decrement}>-</button>
+      {friends.map((f) => (
+        <li key={Math.random()}>{f}</li>
+      ))}
+
+      <button onClick={addOne}>Add new friend</button>
+      <button onClick={removeOne}>Remove friend</button>
+      <button onClick={updateOne}>Update one friend</button>
     </section>
   );
 }

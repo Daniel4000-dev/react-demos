b97c563cd3c4f7ff3f16e817a8970dfc3a117732
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2025-10-14 11:57:18 +0100

    built component for handle state through props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ComponentTwo from "./components/ComponentTwo"
 import CompOne from "./components/CompOne"
 import Exone from "./components/Exone"
@@ -8,11 +8,19 @@ import Exone from "./components/Exone"
 const state = "states are used to store and manage data that chnage over time and affect how components are rendered"
 const hooks = "hooks are special functions added in react 16.8 that allow you to use state and other React features in functional components"
 const usestate = "useState hook allows us to track state in a functional component. state generally refers to data or properties that need to be tracked in an application."
+const useeffect = "useEffect hook allows you to perform side effects in your components. Some example of side effects are fetching data, directly updating the DOM etc."
 function App() {
- const [count, setCount] = useState(0);
+ const [value, setValue] = useState(0);
+
+ useEffect(() =>{
+  console.log("useEffect called");
+  document.title = `Increment ${value}`
+ })
+
   return (
     <section>
-      <Exone />
+      <h2>{value}</h2>
+      <button onClick={() => setValue(value + 1)}>Click</button>
     </section>
   );
 }
